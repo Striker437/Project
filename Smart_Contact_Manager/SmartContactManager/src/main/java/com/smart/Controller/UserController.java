5afb2f03f14136d80afd6a51ad2a01c2aef55b62
Striker437 <shivam.mmmut@gmail.com>
2020-11-01 22:59:48 +0530

    Validation changes in Contact form

--- Smart_Contact_Manager/SmartContactManager/src/main/java/com/smart/Controller/UserController.java
+++ Smart_Contact_Manager/SmartContactManager/src/main/java/com/smart/Controller/UserController.java
@@ -2,9 +2,13 @@ package com.smart.Controller;
 
 import java.security.Principal;
 
+import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,6 +19,7 @@ import com.smart.DAO.ContactRepository;
 import com.smart.DAO.UserRepository;
 import com.smart.Entities.Contact;
 import com.smart.Entities.User;
+import com.smart.Helper.Message;
 
 @Controller
 @RequestMapping("/user")
@@ -69,21 +74,39 @@ public class UserController {
 	
 	//handler for process contact
 	@PostMapping("/process-contact")
-	public String ProcessContact(@ModelAttribute("contact")Contact contact,Principal principle)
+	public String ProcessContact(@Valid @ModelAttribute("contact")Contact contact,BindingResult result1,Principal principle,Model model,HttpSession session)
 	{
 		
+		try
+		{
+		if(result1.hasErrors())
+		{
+			System.out.println("Error "+result1.toString());
+			model.addAttribute("contact", contact);
+			throw new Exception();
+		}
 		String name=principle.getName();
 		//System.out.println("User Detail and Email  "+name);
 		User UserByName=userRepository.getUserByUserName(name);
 		contact.setUser(UserByName);
 		UserByName.getContacts().add(contact);
 		userRepository.save(UserByName);
-		System.out.println("Added"+contact.getName()+contact.getEmail());
+		//System.out.println("Added"+contact.getName()+contact.getEmail());
+		model.addAttribute("contact", new Contact());
+		session.setAttribute("message", new Message("Successfully Added!!","Alert Sucess!!"));
+		return "normal/add_contact_form";
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+			model.addAttribute("contact", contact);
+			session.setAttribute("message",new Message("Something Went Wrong !!", "Alert-Danger!!"));
+			return "normal/add_contact_form";
+		}
 		
 		//System.out.println("DATA  "+contact.toString());
 		
 		
-		return "normal/add_contact_form";
+		
 	}
 
 }
